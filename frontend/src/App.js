d831d8f732ad8f33331699ccd70cb277e6fe944c
OMAR <116152772+Omarelnoamy@users.noreply.github.com>
2025-06-26 21:43:28 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage"; // Correct import path for Dashboard
 import AboutUs from "./pages/AboutUs"; // Correct import path for AboutUs
 import Patients from "./pages/Patients"; // Correct import path for Patients
 import Doctors from "./pages/Doctors"; // Correct import path for Doctors
-import Pharmacists from "./pages/Pharmacists"; // Correct import path for Pharmacists
+//import Pharmacists from "./pages/Pharmacists"; // Correct import path for Pharmacists
 import Login from "./pages/Login"; // Correct import path for Login
 import PatientProfile from "./pages/PatientProfile"; // Adjust path if needed
 
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/doctors" element={<Doctors />} />
-          <Route path="/pharmacists" element={<Pharmacists />} />
+{/* <Route path="/pharmacists" element={<Pharmacists />} />*/}
           <Route path="/login" element={<Login />} />
           <Route path="/patients" element={<Patients />} />
           <Route path="/patients/:id" element={<PatientProfile />} />
